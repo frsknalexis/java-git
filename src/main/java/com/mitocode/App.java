ffb63cc71a51d7f9f33cde90f4f5c84fcc3bffeb
Alexis Gutierrez <alexisguty.2018@gmail.com>
2018-07-29 03:08:48 -0500

    Actualizando el main
    Intentalo de nuevo -.-

--- src/main/java/com/mitocode/App.java
+++ src/main/java/com/mitocode/App.java
@@ -3,7 +3,7 @@ package com.mitocode;
 public class App {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		System.out.println("Hello World no seas cojudo pe :V!");
 	}
 
 }
