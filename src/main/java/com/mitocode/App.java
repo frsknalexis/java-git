9a8dd5e69c80484d1a53a8ea136b6e423c81a15d
frsknalexis <alexisgutierrezf.1997@gmail.com>
2018-07-29 03:25:18 -0500

    Se creo un commit en el main

--- src/main/java/com/mitocode/App.java
+++ src/main/java/com/mitocode/App.java
@@ -3,7 +3,7 @@ package com.mitocode;
 public class App {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World no seas cojudo pe :V!");
+		System.out.println("Hello World estamos en VSCode");
 	}
 
 }
